48e6a0a573349e91d5b9bce63537cac5ce6f4f22
PriscilaSilvaAlves <priscila.contato@live.com>
2021-11-17 19:17:14 -0300

    Delete questao12.c

--- Questao12/questao12.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-	int i=3, *p;
-	p=&i;
-	printf("A variável P contém o endereço de i, o ponteiro de p (*p) acessará o endereço contido na variável p, que por acaso é o endereço de i. \n");
-	printf("p=%p",p);
-}
