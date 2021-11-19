d6b3b1f0232125bc67fbb9d8e44a77f46d59484f
PriscilaSilvaAlves <priscila.contato@live.com>
2021-11-19 13:50:33 -0300

    Update questao19.c

--- Questao19/questao19.c
+++ Questao19/questao19.c
@@ -1,7 +1,8 @@
 #include "gc.h"
 #include <stdio.h>
 int main(){
-	int* variavel = (int*)GC_malloc(sizeof(int));
+	int *variavel;
+	variavel = GC_malloc(sizeof(int));
 	*variavel = 42;
 	printf("Variável = %p \n", variavel);
 	GC_free(variavel);
